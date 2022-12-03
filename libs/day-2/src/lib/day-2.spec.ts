a3136e199b93522d8333d9230190dc5fdc279a3a
Michael Nilsson <michael@nilsson.dev>
2022-12-03 23:31:50 +0100

    Add day 2

---
+++ libs/day-2/src/lib/day-2.spec.ts
@@ -0,0 +1,7 @@
+import { day2 } from './day-2';
+
+describe('day2', () => {
+  it('should work', () => {
+    expect(day2()).toEqual('day-2');
+  });
+});
---
+++ libs/day-2/src/lib/day-2.ts
@@ -0,0 +1,3 @@
+export function day2(): string {
+  return 'day-2';
+}
