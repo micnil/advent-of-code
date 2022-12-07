685e946727af07593f6b61888e42e423ee935662
Michael Nilsson <michael@nilsson.dev>
2022-12-07 23:37:25 +0100

    Command mapping/parsing done

--- src/day-7.ts
+++ src/day-7.ts
@@ -1,22 +1,23 @@
-import { assertDefined } from "./util/assertDefined";
+import { assertDefined } from './util/assertDefined';
 
-type DirectoryName = {
-  name: string,
-}
+type DirectoryCommandOutput = {
+  type: 'DIRECTORY';
+  name: string;
+};
 
-type Directory = DirectoryName & {
+type Directory = DirectoryCommandOutput & {
   type: 'DIRECTORY';
   size: number;
   subDirectories: Directory[];
 };
 
-type File = {
+type FileCommandOutput = {
   type: 'FILE';
   name: string;
-  size: string;
+  size: number;
 };
 
-type ListCommandOutput = File | DirectoryName;
+type ListCommandOutput = FileCommandOutput | DirectoryCommandOutput;
 
 type ChangeDirCommand = {
   type: 'CD';
@@ -30,47 +31,58 @@ type ListCommand = {
 
 type ExectutedCommand = ChangeDirCommand | ListCommand;
 
-
-function *iterateChunksByCharacter(input: T[], char: string): Generator<> {
-
+function* iterateCommandInput(input: string[]): Generator<string[]> {
+  let previousCommandIndex = 0
+  for (let i = 1; i < input.length; i++) {
+    if (input[i].startsWith('$')){
+      yield input.slice(previousCommandIndex, i);
+      previousCommandIndex = i
+    }
+  }
+  yield input.slice(previousCommandIndex);
 }
 
 const toCommandOutput = (line: string): ListCommandOutput => {
   const directoryMatch = line.match(/(dir (?<dirName>.*))/);
-  const fileMatch = line.match(/(?<fileSize>[1-9]+) (?<fileName>.*)/);
+  const fileMatch = line.match(/(?<fileSize>[0-9]+) (?<fileName>.*)/);
   if (directoryMatch?.groups) {
-    directoryMatch?.groups.
+    return { type: 'DIRECTORY', name: directoryMatch.groups.dirName ?? '' };
   } else if (fileMatch?.groups) {
-
+    return {
+      type: 'FILE',
+      name: fileMatch?.groups.fileName,
+      size: parseInt(fileMatch.groups.fileSize),
+    };
   } else {
-    throw new Error(`Unrecognized list command output: ${line}`)
+    throw new Error(`Unrecognized list command output: ${line}`);
   }
-}
+};
 
 const toListCommand = (output: string[]): ListCommand => {
   return {
     type: 'LS',
-    output: []
-  }
-}
+    output: output.map(toCommandOutput),
+  };
+};
 
 const toChangeDirCommand = (args?: string): ChangeDirCommand => {
-  assertDefined(args, 'cd command must be executed with an argument')
+  assertDefined(args, 'cd command must be executed with an argument');
   return {
-    type: "CD",
-    argument: args
-  }
-}
+    type: 'CD',
+    argument: args,
+  };
+};
 
 const toExectutedCommand = (lines: string[]): ExectutedCommand => {
   const commandLine = lines[0];
   const output = lines.slice(1);
 
-  const matchGroups = commandLine.match(/(\$ (?<commandType>[a-z]+))( (?<args>[a-z]+))/)?.groups;
+  const matchGroups = commandLine.match(
+    /(\$ (?<commandType>[a-z]+))( (?<args>.*))?/
+  )?.groups;
   const commandType = matchGroups?.commandType;
   const args = matchGroups?.args;
-
-  switch(commandType) {
+  switch (commandType) {
     case 'ls':
       return toListCommand(output);
     case 'cd':
@@ -78,14 +90,18 @@ const toExectutedCommand = (lines: string[]): ExectutedCommand => {
     default:
       throw new Error(`unrecognized command: ${commandLine}`);
   }
-}
+};
 
 const toExectutedCommands = (lines: string[]): ExectutedCommand[] => {
-
-}
-
+  const commands: ExectutedCommand[] = []
+  for (const commandChunks of iterateCommandInput(lines)) {
+    commands.push(toExectutedCommand(commandChunks))
+  }
+  return commands
+};
 
 export const solveD7P1 = (lines: string[]): string => {
+  const commands = toExectutedCommands(lines);
   return '';
 };
 
