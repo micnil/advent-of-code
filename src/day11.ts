cb82002788c56eecc657f6d9f33d157ebc8ac2cf
Michael Nilsson <michael@nilsson.dev>
2022-12-14 23:14:28 +0100

    WIP parse monkeys

--- src/day11.ts
+++ src/day11.ts
@@ -1,7 +1,37 @@
-export const solveD11P1 = (lines: string[]): string => {
-    return '';
-  };
+type Operation = () => number;
+type Throw = (item: number) => number;
+type Monkey = {
+  id: number;
+  items: number[];
+  inspect: Operation;
+  throw: Throw;
+};
 
-  export const solveD11P2 = (lines: string[]): string => {
-    return '';
-  };
+const chunkBy = function* <T>(list: T[], predicate: (elem: T) => boolean) {
+  let chunk: T[] = [];
+  for (const element of list) {
+    if (predicate(element)) {
+      yield chunk;
+      chunk = [];
+    } else {
+      chunk.push(element);
+    }
+  }
+  yield chunk;
+};
+
+const toMonkey = (lines: string[]): Monkey => {
+
+};
+
+const toMonkeys = (lines: string[]): Monkey[] => {
+  return [...chunkBy(lines, (line) => line === '')].map(toMonkey);
+};
+
+export const solveD11P1 = (lines: string[]): string => {
+  return '';
+};
+
+export const solveD11P2 = (lines: string[]): string => {
+  return '';
+};
